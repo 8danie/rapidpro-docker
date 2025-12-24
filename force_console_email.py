863b7fef86af0644d62b30f1b1dfca1773d1ee55
Daniel <habinezadaniel8@gmail.com>
2025-12-24 10:59:49 +0100

    Configure gitignore

--- force_console_email.py
+++
@@ -1,7 +0,0 @@
-# force_console_email.py
-from django.core.mail.backends.console import EmailBackend
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_HOST = 'localhost'
-EMAIL_PORT = 1025
-SEND_EMAILS = False
--- postgres/init_dbs.sql
+++
@@ -1,8 +0,0 @@
--- On se connecte à la base temba
-\c temba
-
--- Activation des extensions obligatoires pour RapidPro
-CREATE EXTENSION IF NOT EXISTS postgis;
-CREATE EXTENSION IF NOT EXISTS hstore;
-CREATE EXTENSION IF NOT EXISTS pg_trgm;
-CREATE EXTENSION IF NOT EXISTS unaccent;
